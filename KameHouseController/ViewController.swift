708654f7d5c27c6781ef97e2bf0e58499f34c674
Fredy Lievano <fredy.lievano@ogilvy.com.au>
2017-10-18 15:09:03 +1000

    Initial Commit

---
+++ KameHouseController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KameHouseController
+//
+//  Created by Fredy.Lievano on 18/10/17.
+//  Copyright © 2017 Fredy.Lievano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
